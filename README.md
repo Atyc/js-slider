f0d25eb688ab207d2f8decdd15cf1c4e76bdef3a
Octav <octav@lucidgraphics.co.uk>
2015-02-24 13:27:23 +0000

    Added more features, fully functional version

--- README.md
+++ README.md
@@ -4,10 +4,17 @@ It will take DIVs in a container and transform them into a slider.
 
 Features:
 Generates a navigation menu with clickable slide numbers
-
+Keep styling at minimun for easy integration
+Highlight current slide in navigation
+Add left and right arrows to navigation
+Auto Play on page load
+Auto play stop when nav item is clicked
+Added some vars at the top of js file for easy configuration
 
 ToDo:
+Add more vars for config
 Click on image to scroll left and right
-Add left and right arrows
+Add arrows over image
+Stop slide when click on image
 
 ##### &copy; http://www.octav.co.uk
--- slider.js
+++ slider.js
@@ -1,7 +1,8 @@
+
+var slidePause	= 2000;
+var transitionDuration	= 1000;
+
 $(document).ready(function(){
-
-
-
 var visibleSlide;
 if (visibleSlide=="") visibleSilde=1;
 var JSparent=$("#JSlider").parent();
@@ -19,25 +20,49 @@ $("#JSlider").css("width",divWidth);
 JSparent.after(function(){
 	return "<div id='JSliderNavigation'></div>";
 })
+$("#JSliderNavigation").css({"width":JSwidth});
 
-
-//create DIV and components
+//create DIV and components for Nav
+$("#JSliderNavigation").append("<span onClick='navigatePrev()' class='navItem' id='navprev'><</span>");
 for (var i = 1; i<=totalSlides; i++) {
 		$(slides[i-1]).attr("id","JS"+i);
 		$("#JS"+i).css({"float":"left"});
 
 		$("#JSliderNavigation").append("<span onClick='navigateTo("+i+","+JSwidth+")' class='navItem' id='nav"+i+"'>"+i+"</span>");
-
-
+		if (i==1) $("#nav1").addClass("current");
 };
+$("#JSliderNavigation").append("<span onClick='navigateNext()' class='navItem' id='navnext'>></span>");
 
+$("span.navItem").click(function(){
+	clearInterval(thisInterval);
+});
 
 
 });
+var thisInterval = setInterval(function() {navigateNext();}, slidePause);
 
 function navigateTo(slideNo, divWidth) {
 	var duration=1000;
 	$("#JSlider").animate({'margin-left':-slideNo*divWidth+divWidth}, duration);
+	$(".navItem").removeClass("current");
+	$("#nav"+slideNo).addClass("current");
 }
 
+function navigateNext() {	
+	var currentSlide = $('.current').attr('id');
+	if (!currentSlide) currentSlide="nav1";
+	currentSlide=currentSlide.substring(3);
+	var nextSlide=(Number(currentSlide)+1);
+	var JSwidth=$("#JSlider").parent().width();
+	if (nextSlide==7) nextSlide=1;
+	navigateTo(nextSlide, JSwidth) 
+}
 
+function navigatePrev() {		
+	var currentSlide = $('.current').attr('id');
+	currentSlide=currentSlide.substring(3);
+	var prevSlide=(Number(currentSlide)-1);
+	var JSwidth=$("#JSlider").parent().width();
+	if (prevSlide==0) prevSlide=6;
+	navigateTo(prevSlide, JSwidth) 
+}
